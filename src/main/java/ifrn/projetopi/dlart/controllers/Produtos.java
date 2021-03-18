8f61438d14c11b881fbaef43b76ed62381311192
laviniasilva666 <dlavinia2003@gmail.com>
2021-03-18 18:50:30 -0300

    Add layout, listando e detalhando os produtos

--- src/main/java/ifrn/projetopi/dlart/controllers/Produtos.java
+++ src/main/java/ifrn/projetopi/dlart/controllers/Produtos.java
@@ -1,9 +1,15 @@
 package ifrn.projetopi.dlart.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ifrn.projetopi.dlart.models.Produto;
 import ifrn.projetopi.dlart.repositories.ProdutoRepository;
@@ -15,14 +21,51 @@ public class Produtos {
 	private ProdutoRepository pr;
 	
 	@RequestMapping("/")
-	public String GetProdutos() {
-		return "Home";
+	public String index() {
+		return "home";
+	};
+	
+	@GetMapping("/novoproduto")
+	public String novoProd() {
+		return "produto/novoProduto";
 	}
 	
+	@GetMapping("/")
+	public ModelAndView listarProdutos() {
+		
+		List<Produto> produtos = pr.findAll();
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("produtos", produtos);
+		return mv;
+		
+	}
+	
+	@GetMapping("/produto/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Produto> opt = pr.findById(id);
+		
+		if(opt.isEmpty()) {
+			md.setViewName("home");
+			return md;
+		}
+		
+		md.setViewName("produto/detalhes");
+		Produto produto = opt.get();
+		md.addObject("produto", produto);
+		
+		return md;
+		
+		
+		
+	}
+	
+	
+	
 	@PostMapping("/addproduto")
-	public String SaveProduto(Produto produto){
+	public String saveProduto(Produto produto){
 		pr.save(produto);
 		System.out.print("Salvou o produto");
-		return "produtoadd";
+		return "produto/produtoadd";
 	}
 }
