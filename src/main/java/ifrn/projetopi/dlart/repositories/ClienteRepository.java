f251c42f84a07acb94e44b47f3bf4b56a33550b1
xyz <dlavinia2003@gmail.com>
2021-03-20 15:46:37 -0300

    detalhes

--- src/main/java/ifrn/projetopi/dlart/repositories/ClienteRepository.java
+++ src/main/java/ifrn/projetopi/dlart/repositories/ClienteRepository.java
@@ -1,7 +1,6 @@
 package ifrn.projetopi.dlart.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import ifrn.projetopi.dlart.models.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
