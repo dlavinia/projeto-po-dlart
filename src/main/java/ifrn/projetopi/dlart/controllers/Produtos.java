fba09fe740bac148d41d20998cb3239f15466967
laviniasilva666 <dlavinia2003@gmail.com>
2021-03-20 04:45:08 -0300

    Detalhes

--- src/main/java/ifrn/projetopi/dlart/controllers/Produtos.java
+++ src/main/java/ifrn/projetopi/dlart/controllers/Produtos.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -71,12 +72,11 @@ public class Produtos {
 	}
 	//Salvar produtos
 	@PostMapping("/addproduto")
-	public String saveProduto(Produto produto){
+	public String salvarProduto(Produto produto){
 		pr.save(produto);
 		System.out.print("Salvou o produto");
 		return "produto/gerenciarprodutos";
 	}
 	
-
 	
 }
