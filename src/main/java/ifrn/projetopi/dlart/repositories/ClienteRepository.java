ea277628b6cfe05c23dcc8c9733676943872c58d
xyz <dlavinia2003@gmail.com>
2021-03-20 15:33:52 -0300

    Criando Repository para cliente

---
+++ src/main/java/ifrn/projetopi/dlart/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package ifrn.projetopi.dlart.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ifrn.projetopi.dlart.models.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
